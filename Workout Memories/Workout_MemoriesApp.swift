2445de7d9fb978f16f6dea608dae5e6e500d5060
Lonnie Gerol <lonnie@lonniegerol.com>
2021-08-16 21:02:30 -0500

    Initial Commit

---
+++ Workout Memories/Workout_MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_MemoriesApp.swift
+//  Workout Memories
+//
+//  Created by Lonnie Gerol on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Workout_MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
